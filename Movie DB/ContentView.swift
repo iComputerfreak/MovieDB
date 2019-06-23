00906707cd15bbf155c915f7eae52b4564bb9167
Jonas Frey <uni@jonasfrey.de>
2019-06-24 00:14:07 +0200

    Initial Commit

---
+++ Movie DB/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Movie DB
+//
+//  Created by Jonas Frey on 24.06.19.
+//  Copyright © 2019 Jonas Frey. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
