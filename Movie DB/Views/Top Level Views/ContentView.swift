9349e6e5520de00407f51386f76c83c61327cc5f
Jonas Frey <dev@jonasfrey.de>
2020-08-04 17:22:37 +0200

    Re-added ContentView, as it was somehow removed in the previous commit Re-deleted Importer.swift, as it too was somehow re-added in the previous commit

---
+++ Movie DB/Views/Top Level Views/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  Movie DB
+//
+//  Created by Jonas Frey on 24.06.19.
+//  Copyright © 2019 Jonas Frey. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    
+    @ObservedObject private var library = MediaLibrary.shared
+    
+    var body: some View {
+        TabView {
+            LibraryHome()
+                .tabItem {
+                    Image(systemName: "film")
+                    Text("Home")
+                }
+            
+            ProblemsView()
+                .tabItem {
+                    Image(systemName: "exclamationmark.triangle")
+                    Text("Problems")
+                }
+            
+            SettingsView()
+                .tabItem {
+                    Image(systemName: "gear")
+                    Text("Settings")
+                }
+        }
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
