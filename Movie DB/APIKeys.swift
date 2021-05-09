fe82a15cf2d2ede5515e5461d4088dd727b37e85
Jonas Frey <dev@jonasfrey.de>
2021-05-09 22:11:38 +0200

    Added file for API keys

---
+++ Movie DB/APIKeys.swift
@@ -0,0 +1,11 @@
+//
+//  APIKeys.swift
+//  Movie DB
+//
+//  Created by Jonas Frey on 09.05.21.
+//  Copyright © 2021 Jonas Frey. All rights reserved.
+//
+
+struct APIKeys {
+    static let tmdbAPIKey: String = "<INSERT YOUR API KEY HERE>"
+}
