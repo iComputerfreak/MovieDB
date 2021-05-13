84975a83db8497d474642bd3774a71ddf889fe6a
Jonas Frey <dev@jonasfrey.de>
2021-05-13 21:18:38 +0200

    Fixed highlighting issues for seasons and cast

--- Movie DB/Views/Component Views/Detail/BasicInfo.swift
+++ Movie DB/Views/Component Views/Detail/BasicInfo.swift
@@ -17,13 +17,12 @@ struct BasicInfo: View {
             EmptyView()
         } else {
             Section(header: HStack { Image(systemName: "info.circle.fill"); Text("Basic Information") }) {
-                // TODO: Maybe put the ID somewhere at the bottom in a metadata section
-                //Text(mediaObject.id.uuidString)
-                //    .headline("ID")
+                // MARK: Genres
                 if !mediaObject.genres.isEmpty {
                     Text(mediaObject.genres.map(\.name).joined(separator: ", "))
                         .headline("Genres")
                 }
+                // MARK: Overview
                 if let overview = mediaObject.overview, !overview.isEmpty {
                     LongTextView(overview, headlineKey: "Description")
                         .headline("Description")
@@ -31,10 +30,12 @@ struct BasicInfo: View {
                 }
                 // Movie exclusive data
                 if mediaObject.type == .movie, let movie = mediaObject as? Movie {
+                    // MARK: Release Date
                     if let releaseDate = movie.releaseDate {
                         Text(JFUtils.dateFormatter.string(from: releaseDate))
                             .headline("Release Date")
                     }
+                    // MARK: Runtime
                     if let runtime = movie.runtime {
                         if runtime > 60 {
                             let formatString = NSLocalizedString("%lld Minutes (%lldh %lldm)", tableName: "Plurals", comment: "Movie Runtime")
@@ -49,40 +50,43 @@ struct BasicInfo: View {
                 }
                 // Show exclusive data
                 if mediaObject.type == .show, let show = mediaObject as? Show {
-                    // Air date
+                    // MARK: Air date
                     if let firstAirDate = show.firstAirDate,
                        let lastAirDate = show.lastAirDate {
                         // Cast to string to prevent localization
                         Text("\(JFUtils.dateFormatter.string(from: firstAirDate)) - \(JFUtils.dateFormatter.string(from: lastAirDate))" as String)
                             .headline("Air Date")
                     }
-                    // Show type (e.g. Scripted)
+                    // MARK: Show type (e.g. Scripted)
                     if let type = show.showType {
                         Text(NSLocalizedString(type.rawValue))
                             .headline("Show Type")
                     }
                 }
+                // MARK: Status
                 Text(NSLocalizedString(mediaObject.status.rawValue))
                     .headline("Status")
+                // MARK: Original Title
                 Text(mediaObject.originalTitle)
                     .headline("Original Title")
+                // MARK: Original Language
                 Text(JFUtils.languageString(for: mediaObject.originalLanguage) ?? mediaObject.originalLanguage)
                     .headline("Original Language")
-                // Seasons Info
+                // MARK: Seasons
                 if mediaObject.type == .show, let show = mediaObject as? Show, !show.seasons.isEmpty {
                     NavigationLink(destination: SeasonsInfo().environmentObject(mediaObject)) {
                         // Use the highest seasonNumber, not number of elements, since there could be "Specials" seasons which do not count to the normal seasons
                         Text("\(show.seasons.map(\.seasonNumber).max() ?? 0) Seasons", tableName: "Plurals")
                             .headline("Seasons")
-                            .fixHighlighting()
                     }
+                    .fixHighlighting()
                 }
-                // Cast
+                // MARK: Cast
                 if !mediaObject.cast.isEmpty {
                     NavigationLink(destination: CastInfo().environmentObject(mediaObject)) {
                         Text("Cast")
-                            .fixHighlighting()
                     }
+                    .fixHighlighting()
                 }
             }
         }
