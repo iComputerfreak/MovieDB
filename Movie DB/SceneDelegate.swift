f11ec29a64677b8510a4113a430b2e6f8604f7ce
Jonas Frey <dev@jonasfrey.de>
2022-06-03 21:06:49 +0200

    Fixed app not saving when being killed directly, instead of being closed first

--- Movie DB/SceneDelegate.swift
+++ Movie DB/SceneDelegate.swift
@@ -11,11 +11,17 @@ import UIKit
 
 class SceneDelegate: NSObject, UIWindowSceneDelegate {
     func sceneDidEnterBackground(_ scene: UIScene) {
+        print("Scene entered background.")
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
         
         // Save changes in the application's managed object context when the application transitions to the background.
+        PersistenceController.saveContext()
+    }
+    
+    func sceneDidDisconnect(_ scene: UIScene) {
+        // Called when the app is terminated
         PersistenceController.saveContext()
     }
 }
