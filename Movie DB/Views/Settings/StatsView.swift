d51fcca930f88fdd986786f868bfabe0d8fc4971
Jonas Frey <dev@jonasfrey.de>
2024-01-30 17:32:55 +0100

    Removed ListConfigurationView as it is unnecessary

---
+++ Movie DB/Views/Settings/StatsView.swift
@@ -0,0 +1,19 @@
+//
+//  StatsView.swift
+//  Movie DB
+//
+//  Created by Jonas Frey on 30.01.24.
+//  Copyright © 2024 Jonas Frey. All rights reserved.
+//
+
+import SwiftUI
+
+struct StatsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StatsView()
+}
