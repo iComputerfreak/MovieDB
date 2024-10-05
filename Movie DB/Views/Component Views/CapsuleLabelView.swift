7ba9cf9a59d966e420b58e483be46d6097c3cd27
Jonas Frey <dev@jonasfrey.de>
2024-10-05 18:29:43 +0200

    Add favorite and watchlist labels to library list

--- Movie DB/Views/Component Views/CapsuleLabelView.swift
+++ Movie DB/Views/Component Views/CapsuleLabelView.swift
@@ -13,28 +13,63 @@ struct CapsuleLabelView<Content>: View where Content: View {
     @ViewBuilder let content: () -> Content
     @Environment(\.colorScheme) private var colorScheme
     
-    init(text: String, color: Color? = nil) where Content == Text {
-        self.content = {
-            Text(text)
-                .font(.caption)
-                .bold()
-                .foregroundColor(color ?? .primary)
-        }
+    private let color: Color?
+    private let font: Font
+    private let preserveMinimumHeight: Bool
+    
+    init(
+        text: String,
+        font: Font = .caption,
+        color: Color? = nil
+    ) where Content == Text {
+        self.init(
+            color: color,
+            font: font,
+            preserveMinimumHeight: false,
+            content: {
+                Text(text)
+                    .bold()
+            }
+        )
     }
     
-    init(@ViewBuilder content: @escaping () -> Content) {
+    /// Creates a new capsule view with the given content
+    /// - Parameters:
+    ///   - preserveMinimumHeight: Whether the view should be at least as high as text of the current font size
+    ///   - content: The content to display inside the capsule
+    init(
+        color: Color? = nil,
+        font: Font = .caption,
+        preserveMinimumHeight: Bool = false,
+        @ViewBuilder content: @escaping () -> Content
+    ) {
+        self.color = color
+        self.font = font
+        self.preserveMinimumHeight = preserveMinimumHeight
         self.content = content
     }
     
     var body: some View {
-        content()
-            .padding(.horizontal, 5)
-            .padding(.vertical, 1.5)
-            .background(
-                Capsule(style: .continuous)
-                    // Use a gray background, depending on the scheme
-                    .fill(colorScheme == .dark ? Color(white: 0.2) : Color(white: 0.9))
-            )
+        Group {
+            if preserveMinimumHeight {
+                ZStack {
+                    Text(verbatim: "X")
+                        .opacity(0)
+                    content()
+                }
+            } else {
+                content()
+            }
+        }
+        .font(font)
+        .foregroundStyle(color ?? .primary)
+        .padding(.horizontal, 3)
+        .padding(.vertical, 3)
+        .background(
+            Capsule(style: .continuous)
+            // Use a gray background, depending on the scheme
+                .fill(colorScheme == .dark ? Color(white: 0.2) : Color(white: 0.9))
+        )
     }
 }
 
