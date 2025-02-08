82fa6955d725121bee6292a98b97f3f5b5510245
Jonas Frey <dev@jonasfrey.de>
2025-02-08 18:16:33 +0100

    Update static test resources

--- updateStaticPreviewResources.sh
+++ updateStaticPreviewResources.sh
@@ -4,8 +4,6 @@ set -e
 
 cd "$(dirname "$0")"
 
-cd Movie\ DB/Preview\ Content/Resources
-
 function downloadMovie {
 	local id=$1
 	curl --header "Authorization: Bearer $TMDB_API_KEY" -s "https://api.themoviedb.org/3/movie/$id?append_to_response=keywords,translations,videos,watch/providers,credits,external_ids,release_dates&language=en-US&region=US" | jq
@@ -16,14 +14,24 @@ function downloadShow {
 	curl --header "Authorization: Bearer $TMDB_API_KEY" -s "https://api.themoviedb.org/3/tv/$id?append_to_response=keywords,translations,videos,watch/providers,credits,external_ids,content_ratings&language=en-US&region=US" | jq
 }
 
-echo "Updating The Matrix..."
-downloadMovie 603 > Matrix.json
+function update {
+    echo "Updating The Matrix..."
+    downloadMovie 603 > Matrix.json
+    
+    echo "Updating Fight Club..."
+    downloadMovie 550 > FightClub.json
+    
+    echo "Updating Game of Thrones..."
+    downloadShow 1399 > GameOfThrones.json
+    
+    echo "Updating The Blacklist..."
+    downloadShow 46952 > Blacklist.json
+}
 
-echo "Updating Fight Club..."
-downloadMovie 550 > FightClub.json
+cd "$(dirname "$0")/Movie DB/Preview Content/Resources"
+update
 
-echo "Updating Game of Thrones..."
-downloadShow 1399 > GameOfThrones.json
+echo
 
-echo "Updating The Blacklist..."
-downloadShow 46952 > Blacklist.json
+cd "../../../Movie DBTests/Resources"
+update
