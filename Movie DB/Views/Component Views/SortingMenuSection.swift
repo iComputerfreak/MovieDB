c3cdedb5e3389f605e70bc7bf90f8c3290c8eaa6
Jonas Frey <dev@jonasfrey.de>
2025-10-16 10:07:36 +0200

    Use subtitles instead of icons for sorting menu

--- Movie DB/Views/Component Views/SortingMenuSection.swift
+++ Movie DB/Views/Component Views/SortingMenuSection.swift
@@ -12,35 +12,38 @@ import SwiftUI
 struct SortingMenuSection: View {
     @Binding var sortingOrder: SortingOrder
     @Binding var sortingDirection: SortingDirection
-    
+
+    // To allow toggling the sorting direction, we need to use a custom binding as proxy
+    private var sortingOrderProxy: Binding<SortingOrder> {
+        Binding<SortingOrder> {
+            self.sortingOrder
+        } set: { newValue in
+            // Animate the sorting change
+            withAnimation {
+                if self.sortingOrder == newValue {
+                    // Toggle the direction when tapping an already selected item
+                    self.sortingDirection.toggle()
+                } else {
+                    // Otherwise, use the default direction for this sorting order
+                    self.sortingDirection = newValue.defaultDirection
+                }
+                self.sortingOrder = newValue
+            }
+        }
+    }
+
     var body: some View {
         Section {
-            // To allow toggling the sorting direction, we need to use a custom binding as proxy
-            let sortingOrderProxy = Binding<SortingOrder> {
-                self.sortingOrder
-            } set: { newValue in
-                // Animate the sorting change
-                withAnimation {
-                    if self.sortingOrder == newValue {
-                        // Toggle the direction when tapping an already selected item
-                        self.sortingDirection.toggle()
-                    } else {
-                        // Otherwise, use the default direction for this sorting order
-                        self.sortingDirection = newValue.defaultDirection
-                    }
-                    self.sortingOrder = newValue
-                }
-            }
             Picker(selection: sortingOrderProxy) {
                 ForEach(SortingOrder.allCases, id: \.rawValue) { order in
-                    if self.sortingOrder == order {
-                        let image = sortingDirection == .ascending ? "chevron.up" : "chevron.down"
-                        Label(order.localized, systemImage: image)
-                            .tag(order)
-                    } else {
+                    Button {} label: {
                         Text(order.localized)
-                            .tag(order)
+                        // If we have the value currently selected, display the sorting direction as a subtitle
+                        if self.sortingOrder == order {
+                            Text(sortingDirection.localized)
+                        }
                     }
+                    .tag(order)
                 }
             } label: {
                 Text(Strings.Library.menuSortingHeader)
