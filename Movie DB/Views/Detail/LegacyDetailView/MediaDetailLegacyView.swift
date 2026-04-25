088aa8072e48e34021ac0fb5e18a81247546d90d
Jonas Frey <dev@jonasfrey.de>
2026-04-25 14:13:51 +0200

    Re-implement legacy view

--- Movie DB/Views/Detail/LegacyDetailView/MediaDetailLegacyView.swift
+++ Movie DB/Views/Detail/LegacyDetailView/MediaDetailLegacyView.swift
@@ -4,9 +4,78 @@ import SwiftUI
 
 @available(*, deprecated, renamed: "MediaDetail", message: "Use the iOS 26+ variant with a fallback.")
 struct MediaDetailLegacyView: View {
+    @EnvironmentObject private var mediaObject: Media
+    @Environment(\.dismiss) private var dismiss
+
+    // Whether the user is in edit mode right now (editing the user data)
+    // !!!: We cannot use @Environment's \.editMode here since that is meant for list editing (delete, move)
+    // !!!: and therefore would disable all NavigationLinks
+    @State private var isEditing = false
+
     var body: some View {
-        // TODO: Implement
-        Text("TODO: Implement")
+        if mediaObject.isFault {
+            Text(Strings.Detail.errorLoadingText)
+                .navigationTitle(Strings.Detail.navBarErrorTitle)
+        } else {
+            List {
+                LegacyTitleView(media: mediaObject)
+
+                LegacyUserData()
+                    .environment(\.isEditing, isEditing)
+
+                LegacyBasicInfo()
+
+                if !mediaObject.watchProviders.isEmpty {
+                    LegacyWatchProvidersInfo()
+                }
+
+                LegacyTrailersView()
+                LegacyExtendedInfo()
+                LegacyMetadataInfo()
+            }
+            .listStyle(.insetGrouped)
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationTitle(mediaObject.title)
+            .task(priority: .userInitiated) {
+                // If there is no thumbnail, try to download it again
+                // If a media object really has no thumbnail (e.g., link broken), this may be a bit too much...
+                if mediaObject.thumbnail == nil {
+                    mediaObject.loadImages()
+                }
+            }
+            .navigationDestination(for: TagListView.NavigationDestination.self) { _ in
+                TagListView.EditView(tags: $mediaObject.tags)
+            }
+            .toolbar {
+                ToolbarItemGroup(placement: .navigationBarTrailing) {
+                    CustomEditButton(isEditing: $isEditing)
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Menu {
+                        Section {
+                            AddToFavoritesButton()
+                            AddToWatchlistButton()
+                            AddEnvironmentMediaToListMenu()
+                        }
+                        Section {
+                            ReloadMediaButton()
+                            ShareMediaButton()
+                            DeleteMediaButton {
+                                // Dismiss after deleting
+                                dismiss()
+                            }
+                        }
+                    } label: {
+                        MediaMenuLabel()
+                    }
+                }
+            }
+            .onDisappear {
+                if mediaObject.hasChanges {
+                    PersistenceController.saveContext()
+                }
+            }
+        }
     }
 }
 
