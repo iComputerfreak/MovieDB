c83746e615dd7662fcb2208630127a37dc768812
Jonas Frey <dev@jonasfrey.de>
2026-05-06 20:51:35 +0200

    Fix truncation and gap below title view on media detail

--- Movie DB/Views/Detail/DetailView/Sections/ExtendedInfoSection.swift
+++ Movie DB/Views/Detail/DetailView/Sections/ExtendedInfoSection.swift
@@ -5,11 +5,14 @@ import SwiftUI
 struct ExtendedInfoSection: View {
     @EnvironmentObject private var mediaObject: Media
 
+    // .fixedSize below helps prevent truncation in the values due to SwiftUI squishing the texts vertically
+    // This squish for some reason also creates a gap below the title view that leaves the background image visible.
     var body: some View {
         GroupBoxSection(title: Strings.Detail.extendedInfoSectionHeader) {
             // MARK: Tagline
             if let tagline = mediaObject.tagline, !tagline.isEmpty {
                 Text(tagline)
+                    .fixedSize(horizontal: false, vertical: true)
                     .headline(Image(systemName: "bubble.left"), Strings.Detail.taglineHeadline)
             }
 
@@ -17,12 +20,14 @@ struct ExtendedInfoSection: View {
                 // MARK: Budget
                 if movie.budget > 0 {
                     Text(movie.budget.formatted(.currency(code: "USD")))
+                        .fixedSize(horizontal: false, vertical: true)
                         .headline(Image(systemName: "dollarsign"), Strings.Detail.budgetHeadline)
                 }
 
                 // MARK: Revenue
                 if movie.revenue > 0 {
                     Text(movie.revenue.formatted(.currency(code: "USD")))
+                        .fixedSize(horizontal: false, vertical: true)
                         .headline(Image(systemName: "dollarsign"), Strings.Detail.revenueHeadline)
                 }
             }
@@ -30,6 +35,7 @@ struct ExtendedInfoSection: View {
             let tmdbID = mediaObject.tmdbID.description
             if let url = URL(string: "https://www.themoviedb.org/\(mediaObject.type.rawValue)/\(tmdbID)") {
                 Link("\(tmdbID)", destination: url)
+                    .fixedSize(horizontal: false, vertical: true)
                     .headline(Image(systemName: "network"), Strings.Detail.tmdbIDHeadline)
             }
             // MARK: IMDB ID
@@ -38,6 +44,7 @@ struct ExtendedInfoSection: View {
                 let url = URL(string: "https://www.imdb.com/title/\(imdbID)")
             {
                 Link(imdbID, destination: url)
+                    .fixedSize(horizontal: false, vertical: true)
                     .headline(Image(systemName: "network"), Strings.Detail.imdbIDHeadline)
             }
             // MARK: Homepage
@@ -50,6 +57,7 @@ struct ExtendedInfoSection: View {
                     Text(address)
                         .lineLimit(1)
                 }
+                .fixedSize(horizontal: false, vertical: true)
                 .headline(Image(systemName: "network"), Strings.Detail.homepageHeadline)
             }
             // MARK: Production Companies
@@ -60,6 +68,7 @@ struct ExtendedInfoSection: View {
                         .sorted()
                         .formatted()
                 )
+                .fixedSize(horizontal: false, vertical: true)
                 .headline(Image(systemName: "building.2"), Strings.Detail.productionCompaniesHeadline)
             }
             // Show exclusive data
@@ -72,12 +81,14 @@ struct ExtendedInfoSection: View {
                             .sorted()
                             .formatted()
                     )
+                    .fixedSize(horizontal: false, vertical: true)
                     .headline(Image(systemName: "bubble.right"), Strings.Detail.networksHeadline)
                 }
             }
-            let format: FloatingPointFormatStyle<Float> = .number.precision(.fractionLength(2))
             // MARK: Popularity
+            let format: FloatingPointFormatStyle<Float> = .number.precision(.fractionLength(2))
             Text(mediaObject.popularity.formatted(format))
+                .fixedSize(horizontal: false, vertical: true)
                 .headline(Image(systemName: "bubble.right"), Strings.Detail.popularityHeadline)
             // MARK: Score
             // The localized string only shows as many fraction digits as needed, so we can round to at most 2 fraction digits here
@@ -85,6 +96,7 @@ struct ExtendedInfoSection: View {
             let max: Double = 10
             let count = mediaObject.voteCount
             Text(Strings.Detail.scoringValueLabel(avg, max, count))
+                .fixedSize(horizontal: false, vertical: true)
                 .headline(Image(systemName: "bubble.right"), Strings.Detail.scoringHeadline)
         }
         .multilineTextAlignment(.leading)
