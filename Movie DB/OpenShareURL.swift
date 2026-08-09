df6de510f2564e6cbecd5b63e66b60acc940c1a8
Jonas Frey <dev@jonasfrey.de>
2026-08-09 17:26:59 +0200

    Fix missing add and dismiss button on media share receive sheet

--- Movie DB/OpenShareURL.swift
+++ Movie DB/OpenShareURL.swift
@@ -65,11 +65,13 @@ struct ShareDetailView: View {
         // FIX: For some reason, using a NavigationStack here makes the MediaLookupDetail view load indefinitely.
         // NavigationStack {
         if let presentedTMDBID, let presentedTMDBMediaType {
-            MediaLookupDetail(
-                tmdbID: presentedTMDBID,
-                mediaType: presentedTMDBMediaType,
-                showingDismissButton: true
-            )
+            NavigationStack {
+                MediaLookupDetail(
+                    tmdbID: presentedTMDBID,
+                    mediaType: presentedTMDBMediaType,
+                    showingDismissButton: true
+                )
+            }
         } else {
             Text(Strings.ShareDetail.errorLoadingMedia)
                 .onAppear {
